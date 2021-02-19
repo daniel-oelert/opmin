096da65ac04c581059f09b2dee38b060c66c3bcb
- <->
2021-02-19 22:49:30 +0100

    Init.

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask
-
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return 'Hello world'
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+@app.route("/")
+def hello():
+    return "Hello, fuck Digital Ocean!"
+if __name__ == "__main__":
+    app.run()
